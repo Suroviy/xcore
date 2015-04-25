51dcfa317f8862286ff4929b6aded88636bbd059
Suroviy <Syroviy@bk.ru>
2015-04-25 19:18:14 +0400

    Размещение

--- src/Page.php
+++ src/Page.php
@@ -86,4 +86,23 @@ class Page extends Eloquent
         return $this->attributes['url'] = (empty($value)) ? null : $value;
     }
     
+    public static function updateUrlParametr (array $newGet = array())
+    {
+        if (is_array(Input::all()))
+        {
+            $get = array_merge(Input::get(), $newGet);
+            $array = array_where($get, function($key, $value) use ($newGet)
+            {
+                if (isset($newGet[$key]))
+                {
+                    return ($newGet[$key] !== false) ? true : false;
+                }
+                return true;
+            });
+            $param = ($param = http_build_query($array)) ? '?'.$param : null;
+            return \URL::current().$param;
+        }
+        return \URL::current();
+    }
+    
 }
